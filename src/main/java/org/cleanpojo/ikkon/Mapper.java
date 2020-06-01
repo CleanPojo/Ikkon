cf3c8ffec261ed1bf4d5f95cdc427f697d7d7673
gyuwon <gyuwon@live.com>
2020-06-01 23:01:33 +0900

    Initialize build

---
+++ src/main/java/org/cleanpojo/ikkon/Mapper.java
@@ -0,0 +1,9 @@
+package org.cleanpojo.ikkon;
+
+public class Mapper {
+
+    public <T> T map(Object source, Class<T> destination) {
+        return null;
+    }
+    
+}
---
+++ src/test/java/org/cleanpojo/ikkon/Mapper_specs.java
@@ -0,0 +1,10 @@
+package org.cleanpojo.ikkon;
+
+import org.junit.Test;
+
+public class Mapper_specs {
+
+    @Test public void test_runner_works() {
+    }
+    
+}
