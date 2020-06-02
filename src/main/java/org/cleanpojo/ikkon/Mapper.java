60d1fefab6355779b1db1c9a7aaa020affc8612b
gyuwon <gyuwon@live.com>
2020-06-02 22:47:33 +0900

    Refactor Mapper

--- src/main/java/org/cleanpojo/ikkon/Mapper.java
+++ src/main/java/org/cleanpojo/ikkon/Mapper.java
@@ -23,32 +23,44 @@ public class Mapper {
         }
     }
 
-    private <T> T createInstance(Object source, Class<T> destination)
+    private static <T> T createInstance(Object source, Class<T> destination)
             throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         Constructor<?> constructor = getConstructor(destination);
-        Parameter[] parameters = constructor.getParameters();
-        var arguments = new Object[parameters.length];
-        for (int i = 0; i < parameters.length; i++) {
-            Parameter parameter = parameters[i];
-            if (parameter.isNamePresent() == false) {
-                String message = "The parameter does not have a name. Compile your code with '-parameters' option to include parameter names.";
-                throw new RuntimeException(message);
-            }
-            String propertyName = parameter.getName();
-            Method getter = findGetter(source.getClass(), propertyName);
-            if (getter != null) {
-                arguments[i] = getter.invoke(source);
-            }
-        }
+        Object[] arguments = resolveArguments(source, constructor);
         return destination.cast(constructor.newInstance(arguments));
     }
 
-    private <T> Constructor<?> getConstructor(Class<T> destination)
+    private static <T> Constructor<?> getConstructor(Class<T> destination)
             throws NoSuchMethodException {
         return destination.getConstructors()[0];
     }
 
-    private <T> void setProperties(Object source, T instance)
+    private static Object[] resolveArguments(Object source, Constructor<?> constructor)
+            throws IllegalAccessException, InvocationTargetException {
+        Parameter[] parameters = constructor.getParameters();
+        var arguments = new Object[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            arguments[i] = resolveArgument(source, parameters[i]);
+        }
+        return arguments;
+    }
+
+    private static Object resolveArgument(Object source, Parameter parameter)
+            throws IllegalAccessException, InvocationTargetException {
+        String propertyName = getParameterName(parameter);
+        Method getter = findGetter(source.getClass(), propertyName);
+        return getter == null ? null : getter.invoke(source);
+    }
+
+    private static String getParameterName(Parameter parameter) {
+        if (parameter.isNamePresent() == false) {
+            String message = "The parameter does not have a name. Compile your code with '-parameters' option to include parameter names.";
+            throw new RuntimeException(message);
+        }
+        return parameter.getName();
+    }
+
+    private static <T> void setProperties(Object source, T instance)
             throws IllegalAccessException, InvocationTargetException {
         for (Method method : instance.getClass().getMethods()) {
             if (isSetter(method)) {
@@ -58,11 +70,11 @@ public class Mapper {
         }
     }
 
-    private boolean isSetter(Method method) {
+    private static boolean isSetter(Method method) {
         return method.getName().startsWith("set");
     }
 
-    private <T> void setProperty(Object source, T instance, Method setter)
+    private static <T> void setProperty(Object source, T instance, Method setter)
             throws IllegalAccessException, InvocationTargetException {
         String propertyName = setter.getName().substring(3);
         Method getter = findGetter(source.getClass(), propertyName);
@@ -71,7 +83,7 @@ public class Mapper {
         }
     }
 
-    private Method findGetter(Class<?> type, String propertyName) {
+    private static Method findGetter(Class<?> type, String propertyName) {
         for (Method method : type.getMethods()) {
             if (method.getName().equalsIgnoreCase("get" + propertyName)) {
                 return method;
