ec7883a7ee115f1254d575342ada45b49ca1ac91
gyuwon <gyuwon@live.com>
2020-06-02 21:54:00 +0900

    Refactor Mapper

--- src/main/java/org/cleanpojo/ikkon/Mapper.java
+++ src/main/java/org/cleanpojo/ikkon/Mapper.java
@@ -8,22 +8,8 @@ public class Mapper {
 
     public <T> T map(Object source, Class<T> destination) {
         try {
-            Constructor<T> constructor = destination.getConstructor();
-            T instance = constructor.newInstance();
-            for (Method method : destination.getMethods()) {
-                if (method.getName().startsWith("set")) {
-                    Method setter = method;
-                    String propertyName = setter.getName().substring(3);
-                    for (Method sourceMethod : source.getClass().getMethods()) {
-                        if (sourceMethod.getName().equalsIgnoreCase("get" + propertyName)) {
-                            Method getter = sourceMethod;
-                            Object value = getter.invoke(source);
-                            setter.invoke(instance, value);
-                        }
-                    }
-                }
-            }
-
+            T instance = createInstance(destination);
+            setProperties(source, instance);
             return instance;
         } catch (
             InstantiationException
@@ -34,5 +20,49 @@ public class Mapper {
             | SecurityException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    private <T> T createInstance(Class<T> destination)
+            throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        Constructor<T> constructor = getConstructor(destination);
+        return constructor.newInstance();
+    }
+
+    private <T> Constructor<T> getConstructor(Class<T> destination)
+            throws NoSuchMethodException {
+        return destination.getConstructor();
+    }
+
+    private <T> void setProperties(Object source, T instance)
+            throws IllegalAccessException, InvocationTargetException {
+        for (Method method : instance.getClass().getMethods()) {
+            if (isSetter(method)) {
+                Method setter = method;
+                setProperty(source, instance, setter);
+            }
+        }
+    }
+
+    private boolean isSetter(Method method) {
+        return method.getName().startsWith("set");
+    }
+
+    private <T> void setProperty(Object source, T instance, Method setter)
+            throws IllegalAccessException, InvocationTargetException {
+        String propertyName = setter.getName().substring(3);
+        Method getter = findGetter(source.getClass(), propertyName);
+        if (getter != null) {
+            setter.invoke(instance, getter.invoke(source));
+        }
+    }
+
+    private Method findGetter(Class<?> type, String propertyName) {
+        for (Method method : type.getMethods()) {
+            if (method.getName().equalsIgnoreCase("get" + propertyName)) {
+                return method;
+            }
+        }
+
+        return null;
     }
 }
